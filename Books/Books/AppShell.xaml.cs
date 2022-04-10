505fda47ae1383d5d25036231d8d917935aad442
Cat <56826818+Colinkl@users.noreply.github.com>
2022-04-10 16:07:31 +0700

    Добавьте файлы проекта.

---
+++ Books/Books/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Books.ViewModels;
+using Books.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Books
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
