d9afcc1414280119bb1aacb39702a5d00207e29c
Cat <56826818+Colinkl@users.noreply.github.com>
2022-04-19 21:26:07 +0700

    Db optimization

--- Book.Core/Models/Book.cs
+++ Book.Core/Models/Book.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Books.Core.Models
 {
+    [Index(nameof(Title))]
     public class Book
     {
         public int Id { get; set; }
